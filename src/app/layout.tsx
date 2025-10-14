27873b49ed48c6ef087c17a0608973086504eee4
Golam Mustafa <golammustafa@gmail.com>
2025-10-13 22:41:36 -0400

    creating post--sending data to server completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import React from "react";
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         </div>
         {modal}
         {children}
+        <Toaster />
       </body>
     </html>
   );
